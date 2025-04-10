63495a6c23af67b691a3f7d834062ae807d19976
ARSHIT DHIMAN <dhimanarshit99@gmail.com>
2025-04-10 16:32:04 +0530

    update

--- src/Components/About /About.jsx
+++ src/Components/About /About.jsx
@@ -70,6 +70,23 @@ const About = () => {
           <p>I earned my Full Stack Java Development certification from Simplilearn, enhancing my programming expertise!</p>
         </div>
         <hr />
+        <div className="about-achieves">
+          <div className="about-achieve">
+            <h1>Certification of Python</h1>
+            <p>I just received my Python Programming certification from TechGyan! Excited to apply my skills!</p>
+          </div>
+          <hr />
+          <div className="about-achieve">
+            <h1>Certification of Advance Python/</h1>
+            <p>I just earned my Advanced Python Programming certification from TechGyan! Thrilled to enhance my skills!</p>
+          </div>
+          <hr />
+          <div className="about-achieve">
+            <h1>Certification of Artificial Intelligence</h1>
+            <p>I attended an offline bootcamp on Artificial Intelligence at IIT Delhi! What an amazing experience!</p>
+          </div>
+          <hr />
+        </div>
        </div>
     </div>
   );
