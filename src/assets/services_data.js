e202c7585d12add4cefcd39f7499dda9015024af
ARSHIT DHIMAN <dhimanarshit99@gmail.com>
2025-04-10 17:09:20 +0530

    update

--- src/assets/services_data.js
+++ src/assets/services_data.js
@@ -1,33 +1,33 @@
 const Services_Data = [
     {
         s_no:"01",
-        s_name:"Web design",
-        s_desc:"Web development is the process of building, programming..."
+        s_name:"Frontend Development",
+        s_desc:"Building responsive and interactive user interfaces using HTML, CSS, and JavaScript frameworks."
     },
     {
         s_no:"02",
-        s_name:"Graphics design",
-        s_desc:"Web development is the process of building, programming..."
+        s_name:"Backend Development",
+        s_desc:"Creating server-side logic and APIs to power web applications and handle user data securely."
     },
     {
         s_no:"03",
-        s_name:"Social media",
-        s_desc:"Web development is the process of building, programming..."
+        s_name:"Database Management",
+        s_desc:"Designing and managing efficient databases to store and retrieve data seamlessly."
     },
     {
         s_no:"04",
-        s_name:"App design",
-        s_desc:"Web development is the process of building, programming..."
+        s_name:"API Integration",
+        s_desc:"Developing and integrating APIs for smooth communication between services and platforms"
     },
     {
         s_no:"05",
-        s_name:"Digital marketing",
-        s_desc:"Web development is the process of building, programming..."
+        s_name:"Deployment & DevOps",
+        s_desc:"Deploying apps to the cloud and setting up automated workflows for faster updates"
     },
     {
         s_no:"06",
-        s_name:"Content writing",
-        s_desc:"Web development is the process of building, programming..."
+        s_name:"UI/UX Optimization",
+        s_desc:"Enhancing design and usability to create intuitive and user-friendly web experiences."
     },
 ]
  
